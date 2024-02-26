aa15762dc1d9d26d62392832be5c057905545e9c
rbozek <rbozek@gmail.com>
2024-02-26 18:51:39 -0500

    complete all steps up to creating static/image directory

--- finchcollector/urls.py
+++ finchcollector/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# Added include:
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Added
+    path('', include('main_app.urls')),
 ]
