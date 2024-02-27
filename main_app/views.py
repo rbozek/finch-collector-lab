a09befd386796b893a379d82ed6e56dbf8fe3787
rbozek <rbozek@gmail.com>
2024-02-27 15:04:54 -0500

    views.py, import Monkey model, update monkey_index, remove prev temp monkey class & list

--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,8 @@
 from django.shortcuts import render
+from .models import Monkey
 
 # Added
 from django.http import HttpResponse
-
-
-# Adding the Monkey class & list and view function below the imports
-class Monkey:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-monkeys = [
-  Monkey('Alfred', 'fake monkey', 'Loyal servant.', 10),
-  Monkey('Lumiere', 'brass monkey', 'Handy in pre-electricity-grid times.', 100),
-  Monkey('The Wrangler', 'Southwest monkey', 'Don\'t cross this dude.', 4),
-  Monkey('PartyBoy', 'Xmas monkey', 'Will barf on your shoes if he drinks too much eggnog.', 6)
-]
-
 
 # Define the home view
 def home(request):
@@ -28,6 +12,9 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
-# Add new view
 def monkey_index(request):
+  monkeys = Monkey.objects.all()
   return render(request, 'monkeys/monkey-index.html', { 'monkeys': monkeys })
+# From Step 1, refactored to use ORM Monkey mondel! (above)
+# def monkey_index(request):
+#   return render(request, 'monkeys/monkey-index.html', { 'monkeys': monkeys })
