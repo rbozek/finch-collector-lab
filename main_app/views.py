fc09ae91cc598eeae39eb2ece9ac6bad12acd06e
rbozek <rbozek@gmail.com>
2024-02-27 14:59:05 -0500

    Part 2, update home.html, add model to models.py

--- main_app/views.py
+++ main_app/views.py
@@ -22,7 +22,8 @@ monkeys = [
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+  return render(request, 'home.html')
+  # return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
 
 def about(request):
   return render(request, 'about.html')
