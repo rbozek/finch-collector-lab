ab3ca27049635af6f8e1afc060ceb1143f6fc656
rbozek <rbozek@gmail.com>
2024-02-28 17:01:11 -0500

    set up new Brushing model, migrate, set up admin, custom field label

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 # import my models here
-from .models import Monkey
+from .models import Monkey, Brushing
 
 # Register my models here
 admin.site.register(Monkey)
+admin.site.register(Brushing)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+BRUSHINGS = (
+  ('M', 'Morning'),
+  ('A', 'Afternoon'),
+  ('N', 'Night')
+)
+
 class Monkey(models.Model):
   name = models.CharField(max_length=100)
   breed = models.CharField(max_length=100)
@@ -12,3 +18,20 @@ class Monkey(models.Model):
   
   def get_absolute_url(self):
     return reverse('monkey-detail', kwargs={'monkey_id': self.id})
+  
+# Add new Brushing model
+class Brushing(models.Model):
+  date = models.DateField('Brushing date')
+  brushing = models.CharField(
+    max_length=1,
+    # add the 'choices' field option
+    choices=BRUSHINGS,
+    # set the default value for brushing to be 'M''
+    default=BRUSHINGS[0][0]
+  )
+  # Create a monkey_id column in the database
+  monkey = models.ForeignKey(Monkey, on_delete=models.CASCADE)
+  def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+    # questioning my naming choice - "brushing?" "Brush?"
+    return f"{self.get_brushing_display()} on {self.date}"
