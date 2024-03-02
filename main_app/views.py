6cb8d1de1f5c546e475257df173b970d8c6bdfea
rbozek <rbozek@gmail.com>
2024-03-02 16:03:28 -0500

    update MOnkeyCreate view to add auth/user

--- main_app/views.py
+++ main_app/views.py
@@ -48,6 +48,12 @@ class MonkeyCreate(CreateView):
   fields = ['name', 'breed', 'description', 'age']
   # going to use method written in models.py instead:
   # success_url = '/monkeys/' 
+  # PART 7 AUTH - This inherited method is called when a valid cat form is being submitted
+  def form_valid(self, form):
+    # Assign the logged in user (self.request.user)
+    form.instance.user = self.request.user  # form.instance is the monkey
+    # Let the CreateView do its job as usual
+    return super().form_valid(form)
 
 class MonkeyUpdate(UpdateView):
   model = Monkey
