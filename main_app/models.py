92c9222c9db819e5598cd4fe411dac5c4233a286
rbozek <rbozek@gmail.com>
2024-03-02 15:40:27 -0500

    Step 7 - Auth - adjust Monkey model FK user, do migrations

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
+from django.contrib.auth.models import User
 
 
 class Accessory(models.Model):
@@ -27,6 +28,8 @@ class Monkey(models.Model):
   age = models.IntegerField()
   # rename issue??
   accessories = models.ManyToManyField(Accessory)
+  # Add the FK linking to a user instance
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.name
